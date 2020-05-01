d779a080e72120e603cd6121b69a8d4f4b84c11d
ch1nlu <61373452+ch1nlu@users.noreply.github.com>
2020-05-01 14:49:01 -0700

    Add files via upload

---
+++ Project 1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <time.h>
+
+
+using namespace std;
+
+double negExp(double rate);//generate the negative-exp distributed time
+
+
+
+int main()
+{
+    double arrivalRate, serviceRate, MAXBUFFER;
+    srand(time(NULL));
+    cout << "MAXBUFFER size: ";
+    cin >> MAXBUFFER;
+    cout << "Arrival rate: ";
+    cin >> arrivalRate;
+    cout << "Service rate: ";
+    cin >> serviceRate;
+
+
+
+
+    return 0;
+}
+
+double negExp(double rate){
+    double u;
+    u = rand()% RAND_MAX /(double)RAND_MAX;
+    return ((-1 / rate) * log(1 - u));
+}
